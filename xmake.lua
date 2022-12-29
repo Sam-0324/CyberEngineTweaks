c8800b184572e911cfb047f25bbe2c0cdb8cf715
Auska <13802421+effs@users.noreply.github.com>
2022-12-29 20:40:25 +0200

    ➕ Add `fmt` and `scnlib` dependencies for formatting and string parsing

--- xmake.lua
+++ xmake.lua
@@ -57,6 +57,9 @@ add_requires("openrestry-luajit", { configs = { gc64 = true } })
 local imguiUserConfig = path.absolute("src/imgui_impl/imgui_user_config.h")
 add_requires("imgui v1.88-docking", { configs = { wchar32 = true, freetype = true, user_config = imguiUserConfig } })
 
+add_requires("fmt")
+add_requires("scnlib 1.1.2")
+
 target("RED4ext.SDK")
     set_kind("static")
     set_group("vendor")
@@ -74,7 +77,7 @@ target("cyber_engine_tweaks")
     add_headerfiles("src/**.h", "build/CETVersion.h")
     add_includedirs("src/", "build/")
     add_syslinks("User32", "Version", "d3d11", "Dwrite")
-    add_packages("spdlog", "nlohmann_json", "minhook", "hopscotch-map", "imgui", "mem", "sol2", "tiltedcore", "sqlite3", "openrestry-luajit", "xbyak", "stb")
+    add_packages("spdlog", "nlohmann_json", "minhook", "hopscotch-map", "imgui", "mem", "sol2", "tiltedcore", "sqlite3", "openrestry-luajit", "xbyak", "stb", "fmt", "scnlib")
     add_deps("RED4ext.SDK")
     add_configfiles("src/CETVersion.h.in")
 
